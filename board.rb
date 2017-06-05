293b44f2a2326a7aa4593e15dee6d0530b9bcb21
Ali Haq <ali.m.haq@gmail.com>
2017-06-05 16:20:15 -0700

    Work on display/cursor

---
+++ board.rb
@@ -0,0 +1,57 @@
+require_relative 'piece.rb'
+require_relative 'display.rb'
+
+class Board
+  attr_reader :grid
+  def initialize
+    @grid = populate_board
+  end
+
+  def populate_board
+    # rows 0 and 1 => white pieces
+    # rows 6 and 7 => black pieces
+    @grid = Array.new(8) {Array.new(8)}
+    (0..7).each do |row|
+      if row < 2 || row > 5
+        @grid[row] = Array.new(8) {Piece.new}
+      else
+        @grid[row] = Array.new(8) {NullPiece.new}
+      end
+    end
+    @grid
+  end
+
+  def move_piece(start_pos, end_pos)
+
+    # CHANGE TO SYNTACTIC SUGAR INDEXING
+    row1, col1 = start_pos
+    row2, col2 = end_pos
+    if @grid[row1][col1].is_a?(NullPiece)
+      raise ArgumentError.new('No piece at starting position')
+    elsif legal_move?(end_pos)
+      raise ArgumentError.new('Invalid Move')
+    end
+    @grid[row2][col2] = @grid[row1][row1]
+    @grid[row1][row1] = NullPiece.new
+  end
+
+  def legal_move?(end_pos)
+    false
+  end
+
+  def in_bounds?(pos)
+    pos.all? {|el| el.between?(0, 7)}
+  end
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  brd = Board.new
+  disp = Display.new(brd)
+  while true
+    disp.render
+    disp.cursor.get_input
+    # disp.render
+    # 1
+  end
+end
---
+++ display.rb
@@ -0,0 +1,35 @@
+require 'colorize'
+require_relative 'cursor.rb'
+
+class Display
+  attr_reader :cursor, :board
+  def initialize(board)
+    @cursor = Cursor.new([1,1], board)
+    @board = board
+  end
+
+  # def board
+  #   @board = @cursor.board
+  # end
+
+  def render
+    # MAP THE TOP LABELS TO LETTERS
+    puts @cursor.cursor_pos
+    print "   0  1  2  3  4  5  6  7\n"
+    @board.grid.each_with_index do |row, i|
+      print "#{i}  "
+      row.each_with_index do |piece, j|
+        if [i,j] == @cursor.cursor_pos
+          if piece.is_a?(NullPiece)
+            (print "-".colorize(:color => :white, :background => :black) + "  ")
+          else
+            (print "P".colorize(:color => :white, :background => :black) + "  ")
+          end
+        else
+          piece.is_a?(NullPiece) ? (print "-  ") : (print "P  ")
+        end
+      end
+      puts
+    end
+  end
+end
